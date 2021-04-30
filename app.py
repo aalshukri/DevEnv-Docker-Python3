e5d6b2e286454f07160f59cd7e9a819e49c143a8
Ayesh Alshukri <aalshukri@users.noreply.github.com>
2021-04-30 15:43:01 +0100

    Hello world python dev env.

---
+++ app.py
@@ -0,0 +1,18 @@
+from IPython import embed
+#embed()
+
+class PythonDevEnv:    
+
+    def __init__(self):        
+        pass
+
+    def run(self):
+        print("Hello World")
+    #\run()
+
+#\PythonDevEnv
+
+#run
+print("Start")
+pde = PythonDevEnv()
+pde.run()
